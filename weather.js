062ca49004ef9471b199d36cfc8ebd5af102ac31
savion <didiapraisechimeka@gmail.com>
2025-09-14 09:27:35 +0100

    git init

---
+++ weather.js
@@ -0,0 +1,8 @@
+console.log("js is working")
+const units = document.querySelector("#units");
+const searchBar = document.querySelector("#searchBar");
+const searchBtn = document.querySelector("#searchBtn");
+const daysBtn = document.querySelector(".selected");
+const unitsDropdown = document.querySelector("units__filter");
+const searchBarDropdown = document.querySelector(".suggestions");
+const daysBtnDropdown = document.querySelector(".week-days");

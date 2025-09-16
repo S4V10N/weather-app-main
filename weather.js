f8f1633b324977dd0ebcb11f3db44414198e715d
savion <didiapraisechimeka@gmail.com>
2025-09-16 12:37:29 +0100

    once upon at ime

--- weather.js
+++ weather.js
@@ -7,8 +7,16 @@ const unitsDropdown = document.querySelector(".units__filter");
 const searchBarDropdown = document.querySelector(".suggestions");
 const daysBtnDropdown = document.querySelector(".week-days");
 const nodeList = document.querySelectorAll(".filter");
-// const filter = Array.from(nodeList);
+const weekDays = document.querySelectorAll(".weekDays");
+const weekDaysArray = Array.from(weekDays);
+const metricUnit = document.querySelectorAll(".metric");
+const imperialUnit = document.querySelectorAll(".imperial");
+const unitsSwap = document.querySelector(".bold-text");
+const metricUnitArray = Array.from(metricUnit);
+const imperialUnitArray = Array.from(imperialUnit);
 
+console.log(imperialUnitArray)
+console.log(metricUnitArray)
 function swap(one) {
     if(one.style.display === "none" || one.style.display === "") {
         one.style.display = "block";
@@ -21,9 +29,28 @@ function swap(one) {
 units.addEventListener("click", () => swap(unitsDropdown));
 daysBtn.addEventListener("click", () => swap(daysBtnDropdown));
 searchBar.addEventListener("click", () => swap(searchBarDropdown));
-nodeList.forEach(filter => {
-    filter.addEventListener("click", () => {
-        const check = filter.querySelector(".check");
-        check.style.display = check.style.display === "none" ? "inline" : "none"
+
+unitsSwap.addEventListener("click", () => {
+    for (let i = 0; i < metricUnitArray.length; i++) {
+        if (metricUnitArray[i].style.display === "block" || metricUnitArray[i].style.display === "" || metricUnitArray[i].style.display === "inline-block") {
+            metricUnitArray[i].style.display = "none";
+        } else {
+            metricUnitArray[i].style.display = "inline-block";
+        }
+    };
+    for (let i = 0; i < imperialUnitArray.length; i++) {
+        if (imperialUnitArray[i].style.display === "none" || imperialUnitArray[i].style.display === ""){
+            imperialUnitArray[i].style.display = "inline-block";
+        } else {
+            imperialUnitArray[i].style.display = "none";
+        }
+    }
+})
+
+
+weekDaysArray.forEach((dayElement, index) => {
+    dayElement.addEventListener("click", () => {
+        let selected = weekDaysArray[index];
+        daysBtn.textContent = selected.textContent;
     });
 });
